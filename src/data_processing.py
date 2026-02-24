9c7738f7fa6cd5f1d2a8460f50dc14004f2dff03
AndresAvella09 <diegoandresavella@gmail.com>
2026-02-24 18:56:05 -0500

    Métodos faltantes de DataProcessor

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,13 +2,13 @@ class DataProcessor:
     def __init__(self, data):
         self.data = data
 
-#    def clean_data(self):
-#        # Implement data cleaning logic here
-#        pass
-#
-#    def transform_data(self):
-#        # Implement data transformation logic here
-#        pass
+    def clean_data(self):
+        # Implement data cleaning logic here
+        pass
+
+    def transform_data(self):
+        # Implement data transformation logic here
+        pass
 
     def process(self):
         self.clean_data()
